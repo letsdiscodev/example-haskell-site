61a4ada25cf2dfd1a00ed57f73c545cc5e4782f2
gregsadetsky <lepetitg@gmail.com>
2024-08-27 19:38:07 -0400

    .

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,9 @@ import Web.Scotty
 
 main :: IO ()
 main = scotty 3000 $
+  get "/" $ do
+    html "disco haskell ftw!!"
+
   get "/:word" $ do
     beam <- pathParam "word"
     html $ mconcat ["<h1>disco, ", beam, " me up!</h1>"]
